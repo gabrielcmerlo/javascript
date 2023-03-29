5f1fcd11ba439c8510b783653ebd54f1661ea330
Gabriel Costa Merlo <121057663+gabrielcmerlo@users.noreply.github.com>
2023-03-28 22:15:23 -0300

    Else if & switch

--- exercicios/aula12/ex013.js
+++ exercicios/aula12/ex013.js
@@ -1,10 +1,12 @@
 var agora = new Date()
 var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas.`)
-if (hora < 12) {
+if (hora < 12 && hora >= 6) {
     console.log('Bom Dia!')
-} else if (hora <= 18) {
+} else if (hora <= 18 && hora >= 12) {
     console.log('Boa Tarde!')
-} else {
+} else if (hora < 24 && hora >= 19){
     console.log('Boa Noite!')
+} else {
+    console.log('Boa Madrugada!')
 }
