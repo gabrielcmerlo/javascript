d0710f3f16f20cfc7813dc55a33281139a16b9c0
Gabriel Costa Merlo <121057663+gabrielcmerlo@users.noreply.github.com>
2023-04-18 19:52:03 -0300

    Primeira parte do exercício

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,11 @@
-alert('Olá!')
+function contar() {
+    var inicio = document.getElementById('txtinicio')
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('txtpasso')
+    var res = document.getElementById('res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando:'
+    }
+}
