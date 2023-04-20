066592872a98ddefaf344b38f4f930ffef1d7459
Gabriel Costa Merlo <121057663+gabrielcmerlo@users.noreply.github.com>
2023-04-20 11:13:08 -0300

    Exercício tabuada corrigido

--- exercicios/aula14ex/ex018/script.js
+++ exercicios/aula14ex/ex018/script.js
@@ -1,14 +1,17 @@
 function tabuada() {
     let num = document.getElementById('txtn')
-    let res = document.getElementById('txtt')
+    let tab = document.getElementById('selTab')
 
     if (num.value.length == 0) {
         window.alert('Por favor, digite um número!')
     } else {
         let n = Number(num.value)
+        tab.innerHTML = ''
         for(let c = 1; c <= 10; c++) {
-            let s = n * c
-            res.innerHTML += `${n} x ${c} = ${s} `
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `Tab${c}`
+            tab.appendChild(item)
         }
     }
 }
